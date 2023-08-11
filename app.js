ef1c9f4776ac8cb820deb22d96e86531ee143506
Baishali313@gmail.com <Baishali313>
2023-08-11 11:54:55 +0530

    changes done

--- app.js
+++ app.js
@@ -20,4 +20,13 @@ header.style.borderBottom='solid 3px #000';
 
 var item= document.querySelector('.title');
 item.style.color='green';
-item.style.font='bold';
+item.style.fontWeight='bold';
+
+//make third element in the list green backgroud
+
+var items= document.getElementsByClassName('list-group-item');
+items[1].style.backgroundColor='green';
+
+for(var i=0;i<items.length;i++){
+    items[i].style.fontWeight='bold';
+}
