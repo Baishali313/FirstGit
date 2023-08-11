d98d738b06567f6aa0596a520f7bf16e24fc923e
Baishali313@gmail.com <Baishali313>
2023-08-11 14:23:57 +0530

    lecture 6

--- app.js
+++ app.js
@@ -12,25 +12,25 @@
 
 //1. boarder in the title
 //var header= document.getElementById('header-tile');
-var header= document.getElementById('main-header');
+//var header= document.getElementById('main-header');
 
-header.style.borderBottom='solid 3px #000';
+//header.style.borderBottom='solid 3px #000';
 
 //2 make add item green
 
-var item= document.querySelector('.title');
-item.style.color='green';
-item.style.fontWeight='bold';
+//var item= document.querySelector('.title');
+//item.style.color='green';
+//item.style.fontWeight='bold';
 
 //make third element in the list green backgroud
 // assignment 4
 
-var items= document.getElementsByClassName('list-group-item');
-items[1].style.backgroundColor='green';
+//var items= document.getElementsByClassName('list-group-item');
+//items[1].style.backgroundColor='green';
 
-for(var i=0;i<items.length;i++){
-    items[i].style.fontWeight='bold';
-}
+//for(var i=0;i<items.length;i++){
+  //  items[i].style.fontWeight='bold';
+//}
 
 //assignment 5
 
@@ -42,10 +42,49 @@ for(var i=0;i<items.length;i++){
 
 
 //using tagname
-var li= document.getElementsByTagName('li');
-li[4].style.color='green';
-li[4].style.backgroundColor='#f4f4f4';
-li[4].style.fontWeight='bold';
+//var li= document.getElementsByTagName('li');
+//li[4].style.color='green';
+//li[4].style.backgroundColor='#f4f4f4';
+//li[4].style.fontWeight='bold';
+
+//assignment 6
+
+//querySelector and querySelectorAll
+
+var header=document.querySelector('#main-header');
+header.style.borderBottom= 'solid 4px #ccc';
+
+var input= document.querySelector('input');
+input.value='hello world';
+
+var submit= document.querySelector('input[type="submit"]');
+submit.value='SEND';
+
+//var item= document.querySelector('.list-group-item');
+//item.style.color='red';
+
+//var lastitem= document.querySelector('.list-group-item:last-child');
+//lastitem.style.color='blue';
+
+//var seconditem= document.querySelector('.list-group-item:nth-child(2)');
+//seconditem.style.backgroundColor='green';
+
+//var thirditem= document.querySelector('.list-group-item:nth-child(3)');
+//thirditem.textContent='';
+
+//Query Selector All
+//var titles= document.querySelectorAll('.title');
+//console.log(titles);
+//var odd= document.querySelectorAll('li:nth-child(odd');
+//for(var i=0;i<odd.length;i++){
+  //  odd[i].style.backgroundColor='#f4f4f4';
+//}
+
+var output= document.querySelectorAll('.list-group-item');
+output[1].style.color='green';
+output[2].textContent='';
+
+
 
 
 
