17bb1ab372c93ffc2c2c3a675868012bbbd1d077
Baishali313@gmail.com <Baishali313>
2023-08-11 11:30:13 +0530

    javascript file

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-console.log(' i love git');
+//examine the document object
+//console.dir(document);
+
+//console.log(document.URL);
+//console.log(document.title);
+//console.log(document.head);
+//console.log(document.body);
+//console.log(document.all);
+
+
+//console.log(document.forms);
+
+//1. boarder in the title
+//var header= document.getElementById('header-tile');
+var header= document.getElementById('main-header');
+
+header.style.borderBottom='solid 3px #000';
+
+//2 make add item green
+
+var item= document.querySelector('.title');
+item.style.color='green';
+item.style.font='bold';
